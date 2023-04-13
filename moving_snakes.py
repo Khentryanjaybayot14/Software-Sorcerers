698325e29503a3d7fb1773ab7bbef5eb913dd756
c <christymae.baslan@zdspgc.edu.ph>
2023-04-13 19:22:07 +0800

    moving blocks

--- moving_snakes.py
+++ moving_snakes.py
@@ -4,6 +4,11 @@ from pygame.locals import *
 if __name__ == "__main__":
     pygame.init()
 
+    def draw():
+        surface.fill((0,0,0))
+        surface.blit(blue_snake, (blue_snake_x, blue_snake_y))
+        surface.blit(yellow_snake, (yellow_snake_x, yellow_snake_y))
+        pygame.display.flip()
 
     surface = pygame.display.set_mode((1000, 600))
     surface.fill((0,0,0))
@@ -27,6 +32,28 @@ if __name__ == "__main__":
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     running = False
+
+                if event.key == K_w:
+                    blue_snake_y -= 10
+                    draw()
+                if event.key == K_s:
+                    blue_snake_y += 10
+                    draw()
+                if event.key == K_a:
+                    blue_snake_x -= 10
+                    draw()
+                if event.key == K_d:
+                    blue_snake_x += 10
+                    draw()
+
+                if event.key == K_UP:
+                    pass
+                if event.key == K_DOWN:
+                    pass
+                if event.key == K_LEFT:
+                    pass
+                if event.key == K_RIGHT:
+                    pass
 
             elif event.type == QUIT:
                 running = False
